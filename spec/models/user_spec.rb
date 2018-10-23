67cdff85eab1d9c6478a9635da059949af513b5b
Kainoa Ubaldo-Brabo <kubaldobrabo@zendesk.com>
2018-10-22 21:37:35 -0700

    added association to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Category, type: :model do
     it "has_many posts" do
       association = described_class.reflect_on_association(:posts)
       expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
     end
   end
 end
